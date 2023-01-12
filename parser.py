dc05f0a9338da394172c65cc019da5ae282f206b
Jey0 <mathieu.baudon@gmail.com>
2023-01-12 14:53:05 +0100

    Write parse_header, parse_chunk and parse functions
    Entropy variable still must be computed
    
    Some attributes and functions have been deleted as there were not
    useful

--- parser.py
+++ parser.py
@@ -1,34 +1,65 @@
+import binascii
 import sys
 
 class PNGImage(object):
 
     def __init__(self, path):
         self.path = path
-        self.height = 0
-        self.width = 0
-        self.crc = 0
-        
-    def is_png(self):
-        """
-        Check the magic bytes of the file
-        """
-        with open(self.path, "rb") as f:
-            beginning = f.read(8)
-            png_magic_bytes = b"\x89\x50\x4E\x47\x0D\x0A\x1A\x0A"
-            return list(beginning) == list(png_magic_bytes)
+        self.current_offset = 0
 
     def parse_header(self):
-        pass
+        """
+        Parse the header of the PNG file
+        """
+        with open(self.path, "rb") as f:
+            f.seek(8)
+            # Length of the following chunk data
+            chunk_data_length = int(f.read(4).hex(), 16)
 
-    def parse_chunk(self):
-        pass
+            # Chunk type
+            chunk_type = f.read(4)
+
+            # chunk_data
+            chunk_data = f.read(chunk_data_length)
+
+            # read CRC
+            crc = "0x" + f.read(4).hex()
+
+            crc_computed = "0x" + hex(binascii.crc32(chunk_type + chunk_data))
+            entropy = 0
+            self.current_offset = 8 + 4 + 4 + chunk_data_length + 4
+
+            chunk_type = chunk_type.decode()
+            print(chunk_type, chunk_data_length, crc, crc_computed, entropy, sep=",")
+            return chunk_type
+
+
+    def parse_chunk(self, offset):
+        """
+        Parse a chunk of the PNG file
+        """
+        with open(self.path, "rb") as f:
+            f.seek(offset)
+            chunk_data_length = int(f.read(4).hex(), 16)
+            chunk_type = f.read(4)
+
+            chunk_data = f.read(chunk_data_length)
+            crc = "0x" + f.read(4).hex()
+
+            crc_computed = "0x" + hex(binascii.crc32(chunk_type + chunk_data))
+            entropy = 0
+            self.current_offset = offset + 4 + 4 + chunk_data_length + 4
+            
+            chunk_type = chunk_type.decode()
+            print(chunk_type, chunk_data_length, crc, crc_computed, entropy, sep=",")
+            return chunk_type
 
     def parse(self):
-        self.parse_header()
-        self.parse_chunk()
+        print("type, size, CRC, CRC_ref, entropy")
+        parsed_type = self.parse_header()
+        while parsed_type != "IEND":
+            parsed_type = self.parse_chunk(self.current_offset)
 
-    def __repr__(self):
-        pass
 
 def print_help():
     print("Parse the different chunks of a PNG image")
@@ -36,11 +67,25 @@ def print_help():
     print("Usage:")
     print("  python3 ./parser.py [file_path]")
 
+def is_png(path):
+    """
+    Check the magic bytes of the file
+    """
+    with open(path, "rb") as f:
+        beginning = f.read(8)
+        png_magic_bytes = b"\x89\x50\x4E\x47\x0D\x0A\x1A\x0A"
+        return list(beginning) == list(png_magic_bytes)
+
 if __name__ == "__main__":
-    image_path = ""
+
     if len(sys.argv) != 2:
         print_help()
-    else:
-        image_path = sys.argv[1]
+        sys.exit(0)
+
+    image_path = sys.argv[1]
+    if not is_png(image_path):
+        print("Error, not a PNG file")
+        sys.exit(0)
+
     my_png = PNGImage(image_path)
-    print(my_png.is_png())
+    my_png.parse()
