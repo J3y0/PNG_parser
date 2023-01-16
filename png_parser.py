610d00426ea33b986d1aa82a9e82a66029ec986b
Jey0 <mathieu.baudon@gmail.com>
2023-01-16 18:22:11 +0100

    Make the code cleaner

--- png_parser.py
+++ png_parser.py
@@ -2,83 +2,87 @@ import binascii
 import entropy
 import sys
 
+MAGIC_BYTES_LENGTH = 8
+CHUNK_LENGTH_BYTES_LENGTH = 4
+CHUNK_TYPE_BYTES_LENGTH = 4
+CRC_BYTES_LENGTH = 4
 
 class PNGImage(object):
 
     def __init__(self, path):
         self.path = path
-        self.current_offset = 0
+        self._current_offset = 0
+        self._current_chunk_type = ""
 
-    def parse_header(self):
+    def _parse_header(self) -> None:
         """
         Parse the header of the PNG file
         """
         with open(self.path, "rb") as f:
-            f.seek(8)
+            f.seek(MAGIC_BYTES_LENGTH)
             # Length of the following chunk data
-            chunk_data_length = int(f.read(4).hex(), 16)
+            # use struct package
+            chunk_data_length = int(f.read(CHUNK_LENGTH_BYTES_LENGTH).hex(), 16)
 
             # Chunk type
-            chunk_type = f.read(4)
+            chunk_type = f.read(CHUNK_TYPE_BYTES_LENGTH)
 
             # chunk_data
             chunk_data = f.read(chunk_data_length)
 
             # read CRC
-            crc = "0x" + f.read(4).hex()
+            crc = "0x" + f.read(CRC_BYTES_LENGTH).hex()
 
             # Computed CRC and entropy
-            crc_computed = "0x" + hex(binascii.crc32(chunk_type + chunk_data))
+            crc_computed = hex(binascii.crc32(chunk_type + chunk_data))
             entpy = round(entropy.entropy(entropy.create_dict(chunk_data)), 2)
             
-            self.current_offset = 8 + 4 + 4 + chunk_data_length + 4
+            self._current_offset = MAGIC_BYTES_LENGTH + CHUNK_LENGTH_BYTES_LENGTH + CHUNK_TYPE_BYTES_LENGTH + chunk_data_length + CRC_BYTES_LENGTH
+            self._current_chunk_type = chunk_type.decode()
 
-            chunk_type = chunk_type.decode()
-            print(chunk_type, chunk_data_length, crc, crc_computed, entpy, sep=", ")
-            return chunk_type
+            print(self._current_chunk_type, chunk_data_length, crc, crc_computed, entpy, sep=", ")
 
 
-    def parse_chunk(self, offset):
+    def _parse_chunk(self) -> None:
         """
         Parse a chunk of the PNG file
         """
         with open(self.path, "rb") as f:
-            f.seek(offset)
+            f.seek(self._current_offset)
             # Length of the following chunk data
-            chunk_data_length = int(f.read(4).hex(), 16)
+            chunk_data_length = int(f.read(CHUNK_LENGTH_BYTES_LENGTH).hex(), 16)
 
             # Chunk type
-            chunk_type = f.read(4)
+            chunk_type = f.read(CHUNK_TYPE_BYTES_LENGTH)
 
             # Chunk data
             chunk_data = f.read(chunk_data_length)
 
             # Read CRC
-            crc = "0x" + f.read(4).hex()
+            crc = "0x" + f.read(CRC_BYTES_LENGTH).hex()
             
             # Computed CRC and entropy
-            crc_computed = "0x" + hex(binascii.crc32(chunk_type + chunk_data))
+            crc_computed = hex(binascii.crc32(chunk_type + chunk_data))
             entpy = round(entropy.entropy(entropy.create_dict(chunk_data)), 2)
             
-            self.current_offset = offset + 4 + 4 + chunk_data_length + 4
+            self._current_offset = self._current_offset + CHUNK_LENGTH_BYTES_LENGTH + CHUNK_TYPE_BYTES_LENGTH + chunk_data_length + CRC_BYTES_LENGTH
+            self._current_chunk_type = chunk_type.decode()
             
-            chunk_type = chunk_type.decode()
-            print(chunk_type, chunk_data_length, crc, crc_computed, entpy, sep=", ")
-            return chunk_type
+            print(self._current_chunk_type, chunk_data_length, crc, crc_computed, entpy, sep=", ")
 
-    def parse(self):
+    def parse(self) -> None:
         """
         Parse the entire PNG file
         The function prints chunks information as a CSV output:
             type, size, CRC, CRC_ref, entropy
         """
         print("type, size, CRC, CRC_ref, entropy")
-        parsed_type = self.parse_header()
-        while parsed_type != "IEND":
-            parsed_type = self.parse_chunk(self.current_offset)
+        self._parse_header()
+        while self._current_chunk_type != "IEND":
+            self._parse_chunk()
 
 
-def print_help():
+def print_help() -> None:
     """
     Print the help message
     """
@@ -87,18 +91,20 @@ def print_help():
     print("Usage:")
     print("  python3 ./parser.py [file_path]")
 
-def is_png(path):
+def is_png(path: str) -> bool:
     """
     Check if the file pointed out by the path is a PNG
     """
     with open(path, "rb") as f:
-        beginning = f.read(8)
+        beginning = f.read(MAGIC_BYTES_LENGTH)
         png_magic_bytes = b"\x89\x50\x4E\x47\x0D\x0A\x1A\x0A"
         return list(beginning) == list(png_magic_bytes)
 
 if __name__ == "__main__":
+    # use argparse
     if len(sys.argv) != 2:
         print_help()
+        # use main function returning an int
         sys.exit(0)
 
     image_path = sys.argv[1]
